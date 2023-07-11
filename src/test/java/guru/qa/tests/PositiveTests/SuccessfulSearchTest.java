8c5d150fc7b240a115c9c5960ab6422a858cea4b
EphimSh <ephimizm@gmail.com>
2023-07-11 04:06:42 +0300

    update SuccessfulSearchTest.java

--- src/test/java/guru/qa/tests/PositiveTests/SuccessfulSearchTest.java
+++ src/test/java/guru/qa/tests/PositiveTests/SuccessfulSearchTest.java
@@ -3,30 +3,36 @@ package guru.qa.tests.PositiveTests;
 import com.codeborne.selenide.Condition;
 import guru.qa.tests.TestBase;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class SuccessfulSearchTest extends TestBase {
+
+
+    @BeforeEach
+    void setPreconditions(){
+        step("go to primekraft.ru", () ->{
+            open("https://primekraft.ru");
+        });
+    }
     @Test
-    @Tag("search")
-    @Feature("Automation test example")
-    @Story("Successful item searching")
+    @Tags({
+            @Tag("search"),
+            @Tag("smoke")
+    })
+    @Feature("Поиск")
+    @Story("Успешный поиск товара")
     @Owner("EphimSh")
     @Severity(SeverityLevel.CRITICAL)
     @Link(value = "primekraft", url = "https://primekraft.ru")
-    @DisplayName("item search")
+    @DisplayName("Поиск товара 'BCAA'")
     void searchTest() {
-        open("https://primekraft.ru");
-        //remove ad-block
-        executeJavaScript("$('#prime_exitblock').remove()");
         step("Нажать на кнопку поиска", () ->{
             $(".menu-search-button").click();
         });
-        step("Ввести 'BCAA' и нажать на кнопку найти", () ->{
+        step("Ввести 'BCAA' и нажать на кнопку enter", () ->{
             $("#popup-title-search-input").setValue("BCAA").pressEnter();
         });
 
