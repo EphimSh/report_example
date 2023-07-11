4787921fb4dd06cf4d6173c874d54942a916f5c7
EphimSh <ephimizm@gmail.com>
2023-07-11 20:10:28 +0300

    update Authorization.java

--- src/test/java/guru/qa/tests/NegativeTests/Authorization.java
+++ src/test/java/guru/qa/tests/NegativeTests/Authorization.java
@@ -25,7 +25,9 @@ public class Authorization extends TestBase {
     @Link(value = "primekraft", url = "https://primekraft.ru")
     @DisplayName("Авторизация с неправильными данными")
     void wrongCredAuthorization(){
-        open("https://primekraft.ru");
+        step("Перейти на главную страницу", () ->{
+            open("https://primekraft.ru");
+        });
         step("Нажать на кнопку 'Войти'", () ->{
             $(".l-head [href*=auth]").should(visible).click();
         });
@@ -39,22 +41,5 @@ public class Authorization extends TestBase {
         });
     }
 
-    @Test
-    @Tag("smoke")
-    @Feature("Отслеживание заказа")
-    @Story("Отслеживания статуса чужого заказа")
-    @Owner("EphimSh")
-    @Severity(SeverityLevel.CRITICAL)
-    @Link(value = "primekraft", url = "https://primekraft.ru")
-    @DisplayName("Ввод случайного числа в поле ввода order_id")
-    void spyOnOtherCustomersOrderStatus(){
-        open("personal/order/spy/");
-        step("Ввести 124 в поле для ввода id заказа", () ->{
-            $("[name=order_id]").setValue("124").pressEnter();
-        });
-        step("Отслеживать чужой заказ невозможно", () ->{
-            $("div.rsform").shouldNotHave(text("Заказ №124"));
-        });
 
-    }
 }
