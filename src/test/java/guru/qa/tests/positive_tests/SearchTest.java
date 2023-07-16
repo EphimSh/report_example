c18b8ebb776bfc0423b27bec058a08bf605d9591
EphimSh <ephimizm@gmail.com>
2023-07-16 23:02:58 +0300

    update SearchTest.java

--- src/test/java/guru/qa/tests/positive_tests/SearchTest.java
+++ src/test/java/guru/qa/tests/positive_tests/SearchTest.java
@@ -8,34 +8,32 @@ import org.junit.jupiter.api.*;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+@Feature("Поиск")
+@Owner("EphimSh")
+@Link(value = "primekraft", url = "https://primekraft.ru")
+@Tags({
+        @Tag("search"),
+        @Tag("smoke")
+})
 public class SearchTest extends TestBase {
 
-
-
     @Test
-    @Tags({
-            @Tag("search"),
-            @Tag("smoke")
-    })
-    @Feature("Поиск")
     @Story("Успешный поиск товара")
-    @Owner("EphimSh")
     @Severity(SeverityLevel.CRITICAL)
-    @Link(value = "primekraft", url = "https://primekraft.ru")
     @DisplayName("Поиск товара 'BCAA'")
     void searchTest() {
-        step("Перейти на главную страницу", () ->{
+        step("Перейти на главную страницу", () -> {
             open("https://primekraft.ru");
         });
-        step("Нажать на кнопку поиска", () ->{
+        step("Нажать на кнопку поиска", () -> {
             $(".menu-search-button").click();
         });
-        step("Ввести 'BCAA' и нажать на кнопку enter", () ->{
+        step("Ввести 'BCAA' и нажать на кнопку enter", () -> {
             $("#popup-title-search-input").setValue("BCAA").pressEnter();
         });
 
         step("В результатах поиска отображается текст: BCAA ", () -> {
-           $(".b-search-page-block").shouldHave(Condition.text("BCAA"));
+            $(".b-search-page-block").shouldHave(Condition.text("BCAA"));
         });
     }
 }
