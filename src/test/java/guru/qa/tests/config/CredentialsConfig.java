4bfb1424781b86326998b61c76ee93db16bc6fc5
EphimSh <ephimizm@gmail.com>
2023-07-16 23:02:37 +0300

    refactored

--- src/test/java/guru/qa/tests/config/CredentialsConfig.java
+++
@@ -1,9 +0,0 @@
-package guru.qa.tests.config;
-
-
-import org.aeonbits.owner.Config;
-@Config.Sources("classpath:config/my.properties")
-public interface CredentialsConfig extends Config {
-    String login();
-    String password();
-}
